2ad349c2aeca7147a399634ce1ed3489feaff0f5
Ray Yu <zhigang.yu@pactera.com>
2016-02-22 10:53:56 +0800

    add app seed-ui

---
+++ seed-ui/tasks/options/connect.js
@@ -0,0 +1,65 @@
+var auth = require('../helpers/auth');
+var proxy = require('../helpers/proxy');
+
+var config = {
+  /**
+   * --------- ADD YOUR UAA CONFIGURATION HERE ---------
+   *
+   * This uaa helper object simulates NGINX uaa integration using Grunt allowing secure cloudfoundry service integration in local development without deploying your application to cloudfoundry.
+   * Please update the following uaa configuration for your solution
+   */
+  uaa: {
+    clientId: 'clientview',
+    serverUrl: 'https://a9486881-3d6a-4d0f-9617-38c59a612682.predix-uaa.run.aws-usw02-pr.ice.predix.io',
+    defaultClientRoute: '/about',
+    base64ClientCredential: 'Y2xpZW50dmlldzpwcmVkaXg='
+  },
+  /**
+   * --------- ADD YOUR SECURE ROUTES HERE ------------
+   *
+   * Please update the following object add your secure routes
+   *
+   * Note: Keep the /api in front of your services here to tell the proxy to add authorization headers.
+   */
+  proxy: {
+    '/api/view-service(.*)': {
+      url: 'https://predix-views.run.aws-usw02-pr.ice.predix.io/v1$1',
+      instanceId: '3fa09a0e-626e-4d9f-92bc-d929ac262bee'
+    }
+  }
+};
+
+module.exports = {
+  server: {
+    options: {
+      port: 9000,
+      base: 'public',
+      open: true,
+      hostname: 'localhost',
+      middleware: function (connect, options) {
+        var middlewares = [];
+
+        //add predix services proxy middlewares
+        middlewares = middlewares.concat(proxy.init(config.proxy));
+
+        //add predix uaa authentication middlewaress
+        middlewares = middlewares.concat(auth.init(config.uaa));
+
+        if (!Array.isArray(options.base)) {
+          options.base = [options.base];
+        }
+
+        var directory = options.directory || options.base[options.base.length - 1];
+        options.base.forEach(function (base) {
+          // Serve static files.
+          middlewares.push(connect.static(base));
+        });
+
+        // Make directory browse-able.
+        middlewares.push(connect.directory(directory));
+
+        return middlewares;
+      }
+    }
+  }
+};
